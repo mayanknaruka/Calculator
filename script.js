ec83b944ea0d8a28d5f2f66358f44a0c7f2ccc31
mayanknaruka <mayanknaruka47@gmail.com>
2026-05-17 22:52:32 +0530

    half done

---
+++ script.js
@@ -0,0 +1,21 @@
+function add(a,b) {
+    return a + b 
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function  mulitply(a , b) {
+    return a * b
+}
+
+function divide(a,b) {
+    return a/b 
+}
+
+let firstNumber = ""
+
+let operator = ""
+
+let secondNumber = ""
