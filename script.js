222e79e0ec007214ddc1df59d1dd042679483cbd
mayanknaruka <mayanknaruka47@gmail.com>
2026-05-19 11:26:22 +0530

    6 step done

--- script.js
+++ script.js
@@ -1,41 +1,114 @@
-function add(a,b) {
-    return a + b 
+// Math Functions
+
+function add(a, b) {
+    return a + b;
 }
 
 function subtract(a, b) {
-  return a - b;
+    return a - b;
 }
 
-function  mulitply(a , b) {
-    return a * b
+function multiply(a, b) {
+    return a * b;
 }
 
-function divide(a,b) {
-    return a/b 
+function divide(a, b) {
+    return a / b;
 }
 
-let firstNumber = ""
 
-let operator = ""
+// Variables
 
-let secondNumber = ""
+let firstNumber = "";
 
-function operate (firstNumber, operator, secondNumber) {
-    
+let operator = "";
+
+let secondNumber = "";
+
+
+// Select Elements
+
+const display = document.querySelector("#display");
+
+const numberButtons = document.querySelectorAll(".number");
+
+const operatorButtons = document.querySelectorAll(".operator");
+
+const equalsButton = document.querySelector("#equals");
+
+
+// Operate Function
+
+function operate(operator, firstNumber, secondNumber) {
+
     if (operator === "+") {
-        return add(firstNumber, secondNumber)
+        return add(firstNumber, secondNumber);
     }
 
-    else if (operator === "-"){
-        return subtract(firstNumber, secondNumber)
-
-    }
-    else if(operator === "*"){
-        return mulitply(firstNumber, secondNumber)
+    else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
     }
 
-    else if(operator === "/"){
-        return divide(firstNumber, secondNumber)
+    else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    }
+
+    else if (operator === "/") {
+        return divide(firstNumber, secondNumber);
     }
 }
 
+
+// Number Buttons
+
+numberButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        // Store first number
+        if (operator === "") {
+
+            firstNumber += button.textContent;
+
+            display.textContent = firstNumber;
+        }
+
+        // Store second number
+        else {
+
+            secondNumber += button.textContent;
+
+            display.textContent = secondNumber;
+        }
+
+    });
+
+});
+
+
+// Operator Buttons
+
+operatorButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        operator = button.textContent;
+
+    });
+
+});
+
+
+// Equals Button
+
+equalsButton.addEventListener("click", () => {
+
+    const result = operate(
+        operator,
+        Number(firstNumber),
+        Number(secondNumber)
+    );
+
+    display.textContent = result;
+
+});
